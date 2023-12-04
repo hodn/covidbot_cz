bec0fba846db8c92dc767a16fa1b261e3b239cb6
Hoang Doan <hoang.doan@rocketmail.com>
2023-12-04 22:21:18 +0100

    Migration API v2 - Manage Tweets

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const qs = require('qs');
 const addOAuthInterceptor = require('axios-oauth-1.0a').default;
 
 const key = process.env.CONSUMER_KEY;
@@ -28,13 +27,8 @@ exports.handler = function (event, context) {
 
     const status = `Přibylo ${confirmedCases} osob s nově prokázaným COVID-19 (včetně ${reinfections} reinfikovaných). Hospitalizováno je ${hospitalized} pacientů.`;
 
-    client.post('https://api.twitter.com/1.1/statuses/update.json', null, {
-      params: {
-        status: status
-      },
-      paramsSerializer: params => {
-        return qs.stringify(params)
-      }
+    client.post('https://api.twitter.com/2/tweets', {
+      text: status
     })
       .then(function (response) {
         console.log(response);
@@ -45,32 +39,31 @@ exports.handler = function (event, context) {
 
   }
   axios.get('https://onemocneni-aktualne.mzcr.cz/api/v3/zakladni-prehled?page=1&itemsPerPage=100&apiToken=' + apiToken)
-  .then(function (response) {
-    let confirmedCases = parseInt(response.data['hydra:member'][0]['potvrzene_pripady_vcerejsi_den']);
-    const hospitalized = response.data['hydra:member'][0]['aktualne_hospitalizovani'];
-    const date = new Date(response.data['hydra:member'][0]['datum']);
-    const logDate = response.data['hydra:member'][0]['potvrzene_pripady_vcerejsi_den_datum'];
+    .then(function (response) {
+      let confirmedCases = parseInt(response.data['hydra:member'][0]['potvrzene_pripady_vcerejsi_den']);
+      const hospitalized = response.data['hydra:member'][0]['aktualne_hospitalizovani'];
+      const date = new Date(response.data['hydra:member'][0]['datum']);
+      const logDate = response.data['hydra:member'][0]['potvrzene_pripady_vcerejsi_den_datum'];
 
-    axios.get(`https://onemocneni-aktualne.mzcr.cz/api/v3/nakazeni-reinfekce?page=1&itemsPerPage=1&datum%5Bafter%5D=${logDate}&apiToken=${apiToken}`)
-      .then(function (response) {
+      axios.get(`https://onemocneni-aktualne.mzcr.cz/api/v3/nakazeni-reinfekce?page=1&itemsPerPage=1&datum%5Bafter%5D=${logDate}&apiToken=${apiToken}`)
+        .then(function (response) {
 
-        const reinfections = parseInt(response.data['hydra:member'][0]['nove_reinfekce']);
-        confirmedCases += reinfections;
+          const reinfections = parseInt(response.data['hydra:member'][0]['nove_reinfekce']);
+          confirmedCases += reinfections;
 
-        const updateDay = date.getDay();
-        const today = new Date().getDay();
+          const updateDay = date.getDay();
+          const today = new Date().getDay();
 
-        if (updateDay === today) {
-          updateTwitter(confirmedCases, hospitalized, reinfections);
-        } else {
-          updateTwitter('NO_MZCR_DATA', 'NO_MZCR_DATA', 'NO_MZCR_DATA');
-        }
-        
-      })
-  }).catch(function (error) {
-    console.log(error);
-  });
-  
+          if (updateDay === today) {
+            updateTwitter(confirmedCases, hospitalized, reinfections);
+          } else {
+            updateTwitter('NO_MZCR_DATA', 'NO_MZCR_DATA', 'NO_MZCR_DATA')
+          }
+        })
+    }).catch(function (error) {
+      console.log(error);
+    });
+
   return context.logStreamName;
 }
 
